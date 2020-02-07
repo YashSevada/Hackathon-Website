8f8f35fb7725bd22d883b5a44c032a3647340e2b
Yash Sevada <yashsevada2000@gmail.com>
2020-02-07 12:29:56 +0530

    Eureka Hackathon Webite

---
+++ hackathon.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hackathon</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+	<br>
+<header style="margin-left: 1rem;">
+
+&emsp;<img src="https://gifimage.net/wp-content/uploads/2018/11/svg-en-gif-6.gif" style="width: 12%;height: 40%;">
+&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<img src="https://img.devrant.com/devrant/rant/r_208664_Bvxd5.gif" style="width: 20%;height: 1%;">
+&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<img src="https://gifimage.net/wp-content/uploads/2018/11/svg-en-gif-6.gif" style="width: 12%;height: 40%;">
+<h1>Welcome To Eureka Hackathon 2020..!</h1>
+</header>
+
+
+<hr>
+<br>
+<div class="container-fluid" style="margin-left: 2rem;margin-right: 2rem;">
+                <div class="row">
+                    <div class="col-lg-5" style="margin-left:2rem;">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title">
+                                 
+                                  <form>
+
+                                    <label for="room">
+                                    	<h4>Need <span style="color: red;">Help?</span></h4>
+                                    </label>
+                                      
+                                   
+                                      <select name="room" style="margin-left:2rem;padding-right:5rem;">
+                                    	  <option value="room1">Room1</option>
+                                    	  <option value="room2">Room2</option>
+                                      	<option value="room3">Room3</option>
+                                     </select>
+                                    
+                                    <span><button class="btn btn-success" style="margin-left:2rem;padding-right:0.7rem;">Call</button>
+                                   </span>
+                                </form>
+                              
+
+
+                </div>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                              <th></th>
+                                              <th></th>
+                                                <th>Schedule</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th>1</th>
+                                                <td>10:15AM</td>
+                                                <td>Feb 14</td>
+                                                <td>Registration</td>
+                                            </tr>
+                                            <tr>
+                                                
+                                                <th>2</th>
+                                                <td>1:15AM</td>
+                                                <td>Feb 14</td>
+                                                <td>Inauguration Ceremony</td>
+                                            </tr>
+                                            <tr>
+                                                
+                                                <th>3</th>
+                                                <td>4:15AM</td>
+                                                <td>Feb 14</td>
+                                                <td>Valentine's Day Celebration</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-5" style="margin-left:6rem;">
+                        <div class="card">
+                            <div class="card-body" style="text-align: center;">
+                                <h3 class="card-title"></h3>
+
+                                	<h2 style="text-align: center;"><b>Important contacts</b></h2>
+                                	<hr style="color: black;">
+                                	
+                                	<b><label>HealthCare</label></b>
+									<button class="button"><span>Prajakta Kunjir</span></button>
+									<br>
+
+                                    <b><label>Registeration</label></b>
+									<button class="button"><span>Tanmay Nigde</span></button>
+									<br>
+
+									<b><label>Security</label></b>
+									<button class="button"><span>Jagruti Patole</span></button>
+									<br>
+
+                                	<b><label>HelpDesk</label></b>
+                                	<button class="button"><span>Saurabh Chordiya</span></button>	
+									<br>
+
+								    <b><label>Food</label></b>
+								    <button class="button"><span>Amruta Maindarhi</span></button>
+									<br>
+									
+									<b><label>Discipline</label></b>
+									<button class="button"><span>Siyona Rahane</span></button>
+									<br>
+
+									<b><label>Technical Team</label></b>
+									<button class="button"><span>Shahrukh Khan</span></button>
+									<br>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-8" style="margin-left:18rem;margin-top: 2.1rem;">
+                        <div class="card">
+                            <div class="card-body" style="text-align: center;">
+                                <h3 class="card-title"></h3>
+
+                                	<h2 style="text-align: center;"><b>Hackathon Venues</b></h2>
+                                	<hr style="color: black;">
+
+
+                                    <table class="table">
+                                        
+                                            <tr>
+                                                <th style="font-size: 22px;">Venue</th>
+                                                <th style="font-size: 22px;">Team ID</th>
+                                                <th style="font-size: 22px;">Student Coordinator</th>
+                                                <th style="font-size: 22px;">Faculty Coordinator</th>
+                                            </tr>
+
+                                            <tr>
+                                                <td>E105 <br> Digital Electronics Lab</td>
+                                                <td>HAK-0154<br>HAK-0173</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                            
+                                            <tr>    
+                                                <td>E107 (B)<br>EDC Lab</td>
+                                                <td>HAK-0176<br>HAK-0031<br>HAK-0390<br>HAK-397</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+                                            <tr>
+                                                <th>E108(B) <br> TV Lab</th>
+                                                <td>HAK-0072<br>HAK-0043<br>HAK-0197</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+                                                                                        
+                                            <tr>
+                                               	<th>E110 <br> Communication Lab</th>
+                                                <td>HAK-0172<br>HAK-0028<br>Jaisingh Ganesh Waghchoure<br>Vaishnavi Shrivastav<br>HAK-0198</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+											
+											<tr>
+                                               	<th>E115 <br> Project Lab</th>
+                                                <td>HAK-395<br>HAK-393<br>Ankita Ajay Bhasme</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+
+                                            <tr>
+                                                <th>E113 <br> VLSI Lab</th>
+                                                <td>HAK-0056<br>HAK-0002<br>HAK-0025</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+
+											<tr>  
+                                               <th>E116 <br> PL/EL Lab</th>
+                                                <td>HAK-0158<br>HAK-0073<br>HAK-0245<br>HAK-0150</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+                                            <tr>  
+                                               <th>E117 <br> SAL/ADL Lab </th>
+                                                <td>HAK-0243<br>HAK-0045<br>HAK-0266<br>HAK-0251</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+   											<tr>  
+                                               <th>E118 <br> SAL/ADL Lab</th>
+                                                <td>HAK-0145<br>HAK0174<br>HAK0224<br>HAK-0150</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+       										<tr>  
+                                               <th>E125 <br> PL/DS Lab</th>
+                                                <td>HAK-0379<br>HAK-0156<br>HAK-0279<br>HAK-0272</td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+<div class="col-lg-5" style="margin-left:4rem;margin-top: 2rem;">
+                        <div class="card">
+                            <div class="card-body" style="text-align: center;">
+                                <h3 class="card-title"></h3>
+
+                                	<h2 style="text-align: center;">Time Machine</h2>
+                                	<hr style="color: black;">
+
+							<label for="email"><b>If you will get a chance to go in past by travelling through time machine then what will you change?</b></label>
+						    <textarea placeholder="Your Answer"></textarea>
+						    <br>
+                            <button class="button Submit"><b>Submit</b></button>                       
+                           
+                           </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+<div class="col-lg-6" style="margin-left:52.5rem;margin-top: -24.5rem;">
+                        <div class="card">
+                            <div class="card-body" style="text-align: center;">
+                                <h3 class="card-title"></h3>
+
+                                	<h2 style="text-align: center;"><b>Hackathon Feedback Form<b></h2>
+                                	<hr style="color: black;">
+
+                                	<label style="margin-left: 0rem;">Which event did you participated in?</label>
+                                	  <select name="room" style="margin-left:2rem;padding-right:5rem;">
+                                    	  <option value="room1">Hackathon</option>
+                                    	  <option value="room3">Ideathon</option>
+                                     </select>
+                                     <br>
+                                     <br>
+
+                     	      <label for="field1" style="margin-left: 0rem;">What was the best part of the event?</label>
+							  <input type="text" id="field1" name="field1">
+                              <br>
+                              <br>
+
+
+
+                                	<label>Are you satisfied with the event?</label>
+                                   <select name="room" style="margin-left:2rem;padding-right:5rem;">
+                                    	  <option value="room1">Very Satisfied</option>
+                                    	  <option value="room3">Satisfied</option>
+                                          <option value="room1">Not Satisfied</option>
+                                     </select>
+                                     <br>
+                                     <br>
+                              <label for="field2" style="margin-left: 0rem;">What should we change for the next time we hole this or a similar event</label>
+                              <input type="text" id="field2" name="field2">
+                              <br>
+
+                              <button class="button Submit"><b>Submit</b></button>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+<br>
+<br>
+                </div>
+</body>
+</html>
